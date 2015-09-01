ae1e0a768efb5eee1cbc19d0a216d84bcdd1f02b
angel-manuel <spirit5556@hotmail.com>
2015-09-01 14:11:40 +0200

    sndnet.h: Header guard changed

--- include/sndnet.h
+++ include/sndnet.h
@@ -1,4 +1,18 @@
-#ifndef SNDNET_H_
-#define SNDNET_H_
+#ifndef SNDNET_SNDNET_H_
+#define SNDNET_SNDNET_H_
 
-#endif/*SNDNET_H_*/
+/**
+ * Initialization of a node
+ * @param[in] port UDP listening port
+ * @param[out] sns Initialized state(must be already allocated)
+ * @return 0 if OK, -1 otherwise
+ * */
+int sndnet_init(short port, SNState* sns);
+
+/**
+ * Destroyes a node
+ * @param sns State to be destroyed(but not deallocated)
+ * */
+void sndnet_destroy(SNState* sns);
+
+#endif/*SNDNET_SNDNET_H_*/
