2c00abb0eddd562993389cd39ab25677718687db
angel-manuel <spirit5556@hotmail.com>
2015-09-05 14:09:18 +0200

    Message payload recv

--- src/sndnet.c
+++ src/sndnet.c
@@ -6,6 +6,7 @@
 #include <pthread.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -13,11 +14,13 @@
 
 #include "address.h"
 
+#define SNDNET_MAX_MSG_LEN 1000
+
 typedef struct SNMessage_ {
 	unsigned char dst[crypto_box_PUBLICKEYBYTES];
 	unsigned char src[crypto_box_PUBLICKEYBYTES];
 	unsigned short ttl;
-	unsigned int len;
+	uint16_t len;
 } SNMessage;
 
 void sndnet_log(SNState* sns, const char* format, ...);
@@ -125,13 +128,57 @@ void* sndnet_background(void* arg) {
 	socklen_t addrlen = sizeof(rem_addr);
 	int recv_count;
 	SNAddress dst, src;
+	unsigned char* buffer;
+	uint16_t size;
 	
 	assert(sns != 0);
 	
 	do {
 		memset(&msg, 0, sizeof(msg));
 		
-		recv_count = recvfrom(sns->socket_fd, &msg, sizeof(msg), 0, &rem_addr, &addrlen);
+		recv_count = recvfrom(sns->socket_fd, &msg, sizeof(msg), MSG_PEEK, &rem_addr, &addrlen);
+		
+		if(recv_count < sizeof(msg)) {
+			if(recv_count < 0) {
+				sndnet_log(sns, "Error on recvfrom()");
+				return 0;
+			} else if(recv_count) {
+				sndnet_log(sns, "Message too short");
+			}
+			recvfrom(sns->socket_fd, &msg, sizeof(msg), 0, 0, 0);
+			continue;
+		}
+		
+		if(msg.len > SNDNET_MAX_MSG_LEN) {
+			sndnet_log(sns, "Message too large: %zu bytes", msg.len);
+			recvfrom(sns->socket_fd, &msg, sizeof(msg), 0, 0, 0);
+			continue;
+		}
+		
+		size = sizeof(SNMessage) + (size_t)msg.len;
+		
+		sndnet_log(sns, "Allocating %zu bytes", size + 1);
+		buffer = (unsigned char*)malloc(size + 1);
+		
+		if(!buffer) {
+			sndnet_log(sns, "Error on malloc");
+			return 0;
+		}
+		
+		recv_count = recvfrom(sns->socket_fd, buffer, size, 0, 0, 0);
+		
+		if(recv_count < size) {
+			if(recv_count < 0) {
+				sndnet_log(sns, "Error on recvfrom()");
+				return 0;
+			} else if(recv_count) {
+				sndnet_log(sns, "Message shorter than specified in header");
+			}
+			free(buffer);
+			continue;
+		}
+		
+		buffer[size] = '\0';
 		
 		sndnet_address_init(&dst, msg.dst);
 		sndnet_address_init(&src, msg.src);
@@ -140,11 +187,14 @@ void* sndnet_background(void* arg) {
 		"dst = %s\n"
 		"src = %s\n"
 		"ttl = %hu\n"
-		"len = %u\n",
+		"len = %u\n"
+		"buf = %s\n",
 		sndnet_address_tostr(&dst),
 		sndnet_address_tostr(&src),
-		msg.ttl, msg.len);
+		msg.ttl, msg.len, buffer + sizeof(SNMessage));
+		
+		free(buffer);
 	} while(recv_count > 0);
 	
-	return 0;
+	return sns;
 }
