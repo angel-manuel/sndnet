2105091a25565d90bc7c47dc246797903f4d7258
angel-manuel <spirit5556@hotmail.com>
2015-09-23 17:13:49 +0200

    prototype: Command-line args added

--- prototype/main.c
+++ prototype/main.c
@@ -1,21 +1,33 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "address.h"
 #include "message.h"
+#include "realaddress.h"
 #include "sndnet.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+    char* self_addr;
+    uint16_t port;
     sndnet_state_t sns;
     sndnet_addr_t self;
     char line[1024];
     char* command;
 
-    sndnet_address_from_hex(&self, "abcd");
+    if(argc < 3) {
+        fprintf(stderr, "Usage: %s <self_addr> <port>\n", argv[0]);
+        return 1;
+    }
+
+    self_addr = argv[1];
+    sscanf(argv[2], "%hu", &port);
+
+    sndnet_address_from_hex(&self, self_addr);
     
-    sndnet_init(&sns, &self, 7777);
+    sndnet_init(&sns, &self, port);
     
     while(printf("> "), fgets(line, 1024, stdin)) {
         command = strtok(line, " \n");
@@ -51,19 +63,28 @@ int main() {
         }
 
         if(strcmp(command, "insert") == 0) {
-            char *addr;
+            char *addr, *raddr;
             sndnet_addr_t sn_addr;
+            sndnet_realaddr_t sn_raddr;
 
             addr = strtok(0, " \n");
 
             if(!addr) {
-                printf("insert <addr>\n");
+                printf("insert <sn_addr> <addr(ip:port)>\n");
+                continue;
+            }
+
+            raddr = strtok(0, " \n");
+
+            if(!raddr) {
+                printf("insert <sn_addr> <addr(ip:port)>\n");
                 continue;
             }
 
             sndnet_address_from_hex(&sn_addr, addr);
+            sndnet_realaddress_from_str(&sn_raddr, raddr);
 
-            sndnet_router_add(&(sns.router), &sn_addr, 0);
+            sndnet_router_add(&(sns.router), &sn_addr, &sn_raddr);
         }
     }
     
