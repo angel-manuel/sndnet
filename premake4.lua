ec226caa9f137e9a298c8ae84d0f4792f0dcf92a
angel-manuel <spirit5556@hotmail.com>
2015-10-14 14:02:28 +0200

    Adding third_party includes. Mintomic.

--- premake4.lua
+++ premake4.lua
@@ -16,7 +16,7 @@ solution "sndnet"
         kind "StaticLib"
         language "C"
         buildoptions { "-D_POSIX_C_SOURCE=200112L -std=c99 -Werror -Wfatal-errors" }
-        includedirs "include"
+        includedirs {"include", "third_party/include"}
         files { "src/**.c"}
 
     project "prototype"
@@ -25,7 +25,7 @@ solution "sndnet"
         language "C"
         buildoptions { "-std=c99 -Werror -Wfatal-errors" }
         links { "sndnet", "pthread" }
-        includedirs "include"
+        includedirs {"include", "third_party/include"}
         files { "prototype/**.c" }
 
     project "test"
@@ -33,5 +33,5 @@ solution "sndnet"
         kind "ConsoleApp"
         language "C++"
         links { "sndnet", "pthread" }
-        includedirs "include"
+        includedirs {"include", "third_party/include"}
         files { "test/**.hpp", "test/**.cpp" }
